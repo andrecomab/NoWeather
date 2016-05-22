5ce8b5c31994f4304c8f2a30e0118df103c7d4f6
Colt <Cewlt@users.noreply.github.com>
2016-05-22 18:58:16 -0400

    Update NoWeather.java

--- com/colt/NoWeather/NoWeather.java
+++ com/colt/NoWeather/NoWeather.java
@@ -4,39 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoWeather extends JavaPlugin implements Listener {
-	
-	private List<String> disabledWorlds;
-	
+
+	private List<String> worlds = new ArrayList<String>();;
+
 	public void onEnable() {
-       		Bukkit.getPluginManager().registerEvents(this, this);
+		Bukkit.getPluginManager().registerEvents(this, this);
 		saveDefaultConfig();
-		
-		disabledWorlds = new ArrayList<>();
-		for(String world : getConfig().getStringList("worlds")) {
-        		if(Bukkit.getWorld(world) != null) {
-        			disabledWorlds.add(world);
-        		}
-		 }
+
+		for (String world : getConfig().getStringList("worlds")) {
+			if (Bukkit.getWorld(world) == null) {
+				getLogger().info("Invalid world in config: " + world);
+			} else {
+				worlds.add(world);
+			}
+		}
 	}
-    
+
 	@EventHandler
-	public void weatherChange(WeatherChangeEvent event) {
-		if(event.toWeatherState()) {
-			for(String w : disabledWorlds) {
-				World world = Bukkit.getWorld(w);
-				if(event.getWorld() == world) {
-					event.setCancelled(true);
-					world.setStorm(false);
-					world.setThundering(false);
-					world.setWeatherDuration(0);
-				}
+	public void WeatherChangeEvent(WeatherChangeEvent event) {
+		if (event.toWeatherState()) {
+			if (worlds.contains(event.getWorld().getName())) {
+				event.setCancelled(true);
 			}
 		}
 	}
