7c99e9ac9dca384217ff3efef81bbf4bdc54855d
andrecomab <andremail2015@libero.it>
2016-06-06 22:15:22 +0200

    Update NoWeather.java

--- com/colt/NoWeather/NoWeather.java
+++ com/colt/NoWeather/NoWeather.java
@@ -3,23 +3,7 @@ package com.colt.noweather;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.weather.WeatherChangeEvent;
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class NoWeather extends JavaPlugin implements Listener {
-
-	private List<String> worlds = new ArrayList<String>();
-
-	public void onEnable() {
-		Bukkit.getPluginManager().registerEvents(this, this);
-		saveDefaultConfig();
-
-		for (String world : getConfig().getStringList("worlds")) {
-			if (Bukkit.getWorld(world) == null) {
-				getLogger().info("Invalid world in config: " + world);
+			if (Bukkit.getWorld(world) == null) {ld in config: " + world);
 			} else {
 				worlds.add(world);
 			}
@@ -28,10 +12,7 @@ public class NoWeather extends JavaPlugin implements Listener {
 
 	@EventHandler
 	public void WeatherChangeEvent(WeatherChangeEvent event) {
-		if (event.toWeatherState()) {
+		if (event.toWeathe()) {
 			if (worlds.contains(event.getWorld().getName())) {
-				event.setCancelled(true);
-			}
-		}
-	}
+				event.setC
 }
